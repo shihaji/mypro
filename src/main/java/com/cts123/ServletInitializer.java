41ea8ca048ae7a426823fdb56e2e08d8fc1ae1be
shihaji <shihajig@gmail.com>
2021-03-01 19:42:37 +0530

    initial commit

---
+++ src/main/java/com/cts123/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cts123;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyproApplication.class);
+	}
+
+}
